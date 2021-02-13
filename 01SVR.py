60d9f4645a17347732523ae7e1d6873478a4f05c
ChemIter2018 <42182243+ChemIter2018@users.noreply.github.com>
2021-02-13 12:32:49 +0800

    SVR

--- 01SVR.py
+++ 01SVR.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+
 from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
@@ -11,7 +13,8 @@ start_time = time()
 FurnaceData = pd.read_csv("00FurnaceCleanData.csv")
 FurnaceDataX = FurnaceData.iloc[:, 1:20]
 FurnaceDataX_Scale = preprocessing.scale(FurnaceDataX)
-FurnaceDataPE = FurnaceData.iloc[:, 26]*100
+# If value < 1, SVR Can't calculate normally, So multiply the value by 1000.
+FurnaceDataPE = FurnaceData.iloc[:, 26]*1000
 
 # Split Train and Test Data
 # random_state: RandomState instance or None, default=None
@@ -28,15 +31,17 @@ y_test_predict = svm_poly_reg.predict(X_test)
 print(r2_score(y_test, y_test_predict))
 
 # Plot
-plt.figure(figsize=(9, 9))
-plt.plot(y_test/100, y_test_predict/100, '.')
-plt.xlabel('Measured Value')
-plt.ylabel('Predicted Value')
-plt.title("P/E", fontsize=20)
+sns.set_theme(style='darkgrid', font='Arial', font_scale=1.5)
+plt.subplots(figsize=(16, 16))
+sns.scatterplot(x=y_test/1000, y=y_test_predict/1000)
+sns.despine(trim=True, left=True)
+plt.xlabel('Measured Value', fontsize=30, fontweight='bold')
+plt.ylabel('Predicted Value', fontsize=30, fontweight='bold')
+plt.xticks(fontsize=30, fontweight='bold')
+plt.yticks(fontsize=30, fontweight='bold')
 plt.savefig('01SVR_PE.png', dpi=600)
 
 # Run Time
 finish_time = time()
 run_time = finish_time - start_time
 print("Total program running time: ", run_time)
-
