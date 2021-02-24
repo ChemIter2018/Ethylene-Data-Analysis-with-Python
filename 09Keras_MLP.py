72fa8be52b15d977eb473e20d9f85ef7f2709334
ChemIter2018 <42182243+ChemIter2018@users.noreply.github.com>
2021-02-25 00:21:48 +0800

    Update 09Keras_MLP.py

--- 09Keras_MLP.py
+++ 09Keras_MLP.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import numpy as np
-import tensorflow as tf
 import pickle
 
 from tensorflow import keras
@@ -21,11 +20,6 @@ start_time = time()
 # Read Furnace Data
 FurnaceData = pd.read_csv("00FurnaceCleanData.csv")
 FurnaceDataX = FurnaceData.iloc[:, 1:20]
-ScalerSave = preprocessing.StandardScaler().fit(FurnaceDataX)
-FurnaceDataX_Scale = ScalerSave.transform(FurnaceDataX)
-pickle.dump(ScalerSave, open('Saved_Model/Keras_MLP_ScalerSave.pkl', 'wb'))
-# ScalerSave = pickle.load(open('Saved_Model/Keras_MLP_ScalerSave.pkl', 'rb'))
-# FurnaceDataX_Scale = ScalerSave.transform(FurnaceDataX)
 
 # If value < 1, MLP Can't calculate normally, So multiply the value by 100.
 n = 100
@@ -33,58 +27,65 @@ FurnaceDataPE = FurnaceData.iloc[:, 26] * n
 
 # Split Train and Test Data
 # random_state: RandomState instance or None, default=None
-X_train, X_test, y_train, y_test = train_test_split(FurnaceDataX_Scale, FurnaceDataPE, random_state=0, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(FurnaceDataX, FurnaceDataPE, random_state=0, test_size=0.3)
 
-np.random.seed(42)
-tf.random.set_seed(42)
+# StandardScaler Data
+ScalerSave = preprocessing.StandardScaler().fit(FurnaceDataX)
+# ScalerSave = pickle.load(open('Saved_Model/Keras_MLP_ScalerSave.pkl', 'rb'))
+X_train = ScalerSave.transform(X_train)
+X_test = ScalerSave.transform(X_test)
+pickle.dump(ScalerSave, open('Saved_Model/Keras_MLP_ScalerSave.pkl', 'wb'))
 
+# Training Model
 model = keras.models.Sequential([
     keras.layers.Dense(30, activation="relu", input_shape=X_train.shape[1:], name="layer1"),
     keras.layers.Dense(15, activation="relu", name="layer2"),
     keras.layers.Dense(1, name="layer3")
 ])
-
 model.compile(loss="mean_squared_error", optimizer=keras.optimizers.Adam(lr=1e-3), metrics=["mean_squared_error"])
-history = model.fit(X_train, y_train, epochs=1000)
+model.fit(X_train, y_train, epochs=1000)
 
-# Save Model
+# Save or Load Model
 model.save('Saved_Model/Keras_MLP')
 # model = tf.keras.models.load_model('Saved_Model/Keras_MLP')
 
 y_test_predict = model.predict(X_test)
+R2 = r2_score(y_test, y_test_predict)
 
-print(r2_score(y_test, y_test_predict))
-
-# Plot
+# Plot Q-Q
 sns.set_theme(style='darkgrid', font='Arial', font_scale=1.5)
 plt.subplots(figsize=(16, 13))
 
-y_test = np.array(y_test/n)
-y_test_predict = y_test_predict.flatten()/n
+y_test = np.array(y_test / n)
+y_test_predict = y_test_predict.flatten() / n
 
 g = sns.scatterplot(x=y_test, y=y_test_predict)
 g.set(xlim=(0.48, 0.55))
 g.set(ylim=(0.48, 0.55))
 
-sns.histplot(x=y_test, y=y_test_predict, bins=50, pthresh=.1, cmap="mako",
-             cbar=True, cbar_kws=dict(shrink=.75))
+h = sns.histplot(x=y_test, y=y_test_predict, bins=50, pthresh=.1, cmap="mako",
+                 cbar=True, cbar_kws=dict(shrink=1))
+
 sns.kdeplot(x=y_test, y=y_test_predict, levels=5, color="w", linewidths=1.5)
 
 sns.despine(trim=True, left=True)
 
 qq = np.linspace(np.min((y_test.min(), y_test_predict.min())),
                  np.max((y_test.max(), y_test_predict.max())))
-plt.plot(qq, qq, color="navy", ls="--", linewidth=2)
-
-plt.text(0.483, 0.545, "$\mathregular{R^2}$" + " = "
-         + str(r2_score(y_test, y_test_predict)), fontsize=20, fontweight='bold')
-plt.xlabel('Measured Value', fontsize=25, fontweight='bold')
-plt.ylabel('Predicted Value', fontsize=25, fontweight='bold')
-plt.xticks(fontsize=25, fontweight='bold')
-plt.yticks(fontsize=25, fontweight='bold')
-plt.savefig('Pictures/09TF_MLP_PE.png', dpi=600)
+plt.plot(qq, qq, color="navy", ls="--", linewidth=2.5)
 
 # Run Time
 finish_time = time()
 run_time = finish_time - start_time
-print("Total program running time: ", run_time)
+
+plt.text(0.482, 0.5405, "$\mathregular{R^2}$" + " = "
+         + str(round(R2, 4)), fontsize=25, fontweight='bold')
+plt.text(0.482, 0.544, "Training time: "
+         + str(round(run_time, 4)) + "s", fontsize=25, fontweight='bold')
+plt.text(0.482, 0.547, "Modeling method: " + "MLP(Keras)", fontsize=25, fontweight='bold')
+
+plt.xlabel('Measured Value', fontsize=30, fontweight='bold')
+plt.ylabel('Predicted Value', fontsize=30, fontweight='bold')
+plt.xticks(fontsize=30, fontweight='bold')
+plt.yticks(fontsize=30, fontweight='bold')
+plt.savefig('Pictures/09TF_MLP_PE.png', dpi=1000)
